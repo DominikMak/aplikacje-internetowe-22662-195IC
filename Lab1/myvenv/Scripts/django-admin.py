8c581dedfb992c217aa5269a1b8df335888ac5ef
DominikMak <dosiu.dm@gmail.com>
2021-11-02 12:27:43 +0100

    Django Blog

--- Lab1/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\asus\Documents\GitHub\aplikacje-internetowe-22662-195IC\Lab1\myvenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
